35a163f9c18333dec75e2585d725178a2f0786ad
Aksh-Konda <aksh.konda@gmail.com>
2020-09-06 21:42:13 +0530

    Corrections

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
 
-app.use(express.static(__dirname+'/../public'));
+app.use(express.static(__dirname+'/public'));
 
 app.use((req, res, next) => {
     res.statusCode = 200;
